ddbbb7cfddf89e89a0ebef565978176dc9cb9381
Remulo Costa <remulo.costa@gmail.com>
2020-04-23 22:47:02 -0300

    stylized dashboard, conected api, saving to storage

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { FiChevronLeft } from 'react-icons/fi';
+import { useRouteMatch, Link } from 'react-router-dom';
+
+import logoImg from '../../assets/logo.svg';
+import { Header } from './style';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Explore repositórios no Github</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  // return <h1>Repository: {params.repository} </h1>;
+
+  return (
+    <Header>
+      <img src={logoImg} alt="Github Explorer" />
+
+      <Link to="/dashboard">
+        <FiChevronLeft size={16} />
+        Voltar
+      </Link>
+    </Header>
+  );
 };
 
 export default Repository;
